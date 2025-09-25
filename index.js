31862cd5f34281069721cbc230af86396ee0fda5
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-25 21:43:06 +0545

    index.js file modified

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+const config = require('./src/configs/config');
+app.use(express.json());
+const db = require('./src/configs/db');
+const port = config.PORT;
+
+db.connect();
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
