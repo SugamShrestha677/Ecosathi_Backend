3d0ed2dba25b35f3814499ae28291af4d5faaa93
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-27 21:51:58 +0545

    myself tried

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.use(express.json());
 const db = require('./src/configs/db');
 const port = config.PORT;
 
-db.connect();
+// db.connect();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World!!!")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    
 });
--- src/configs/config.js
+++ src/configs/config.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
-    JWT_SECRET: process.env.JWT_SECRET,
-    CORS_ORIGIN : process.env.CORS_ORIGIN || "http://localhost:3000,",
-    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3000/api",
-    FRONTEND_BASE_URL: process.env.FRONTEND_BASE_URL || 'http://localhost:3000',
+const config = {
+    PORT:process.env.PORT,
+    JWT_SECRET:process.env.JWT_SECRET,
+    JWT_EXPIRATION:process.env.JWT_EXPIRATION,
+    CORS_ORIGIN:process.env.CORS_ORIGIN,
+    API_BASE_URL:process.env.API_BASE_URL,
+    FRONTEND_BASE_URL:process.env.FRONTEND_BASE_URL
 };
+module.exports = config;
